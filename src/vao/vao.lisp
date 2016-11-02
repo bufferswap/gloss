c30c78a989b903f2c5058232aa75e42ef5bb2fe1
Michael Fiano <michael.fiano@gmail.com>
2016-11-02 13:46:03 -0400

    Added a struct to store attributes, and a function to test if a given attribute type is legal with the OpenGL version requested.

--- src/vao/vao.lisp
+++ src/vao/vao.lisp
@@ -1,1 +1,26 @@
 (in-package :gloss.vao)
+
+(defvar *attrs* (make-hash-table))
+
+(defstruct attr-data
+  (type :float)
+  (count 1)
+  (normalizep nil)
+  (divisor 0)
+  (accessors nil))
+
+(defun attr-type-valid-p (type gl-version)
+  "Check whether the given attribute type and OpenGL version is legal."
+  (let ((validity-table
+          '((:half-float . 3.0)
+            (:float . 3.0)
+            (:byte . 3.0)
+            (:unsigned-byte . 3.0)
+            (:short . 3.0)
+            (:unsigned-short . 3.0)
+            (:int . 3.0)
+            (:unsigned-int . 3.0)
+            (:int-2-10-10-10-rev . 3.2)
+            (:unsigned-int-2-10-10-10-rev . 3.2)
+            (:unsigned-int-10f-11f-11f-rev . 4.4))))
+    (>= (cdr (assoc type validity-table)) gl-version)))
