6fb187056f98bcc7feae654c18fe577647236a36
Michael Fiano <michael.fiano@gmail.com>
2016-11-03 03:18:20 -0400

    Added a reminder for type-checking after the parser is written

--- src/vao/vao.lisp
+++ src/vao/vao.lisp
@@ -19,18 +19,17 @@
           :do (setf (gethash name attr-set) attr))
     attr-set))
 
-(defun attr-type-valid-p (type gl-version)
-  "Check whether the given attribute type and OpenGL version is legal."
-  (let ((validity-table
-          '((:half-float . 3.0)
-            (:float . 3.0)
-            (:byte . 3.0)
-            (:unsigned-byte . 3.0)
-            (:short . 3.0)
-            (:unsigned-short . 3.0)
-            (:int . 3.0)
-            (:unsigned-int . 3.0)
-            (:int-2-10-10-10-rev . 3.2)
-            (:unsigned-int-2-10-10-10-rev . 3.2)
-            (:unsigned-int-10f-11f-11f-rev . 4.4))))
-    (>= (cdr (assoc type validity-table)) gl-version)))
+;; type-checking validity table
+;; this is just to remind what needs to be type-checked later on
+(defvar *attr-validity-table*
+  '((:half-float . (:version 3.0))
+    (:float . (:version 3.0))
+    (:byte . (:version 3.0))
+    (:unsigned-byte . (:version 3.0))
+    (:short . (:version 3.0))
+    (:unsigned-short . (:version 3.0))
+    (:int . (:version 3.0))
+    (:unsigned-int . (:version 3.0))
+    (:int-2-10-10-10-rev . (:version 3.2 :count 4))
+    (:unsigned-int-2-10-10-10-rev . (:version 3.2 :count 4))
+    (:unsigned-int-10f-11f-11f-rev . (:version 4.4 :count 3))))
