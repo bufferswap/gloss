860079ddcd23fa5a001f37670f7463971517c597
Michael Fiano <michael.fiano@gmail.com>
2016-11-02 23:19:58 -0400

    Creation of 'attribute sets'. Still need to validate them.

--- src/vao/vao.lisp
+++ src/vao/vao.lisp
@@ -1,13 +1,23 @@
 (in-package :gloss.vao)
 
-(defvar *attrs* (make-hash-table))
-
-(defstruct attr-data
+(defstruct (attr (:constructor %make-attr))
   (type :float)
   (count 1)
   (normalizep nil)
   (divisor 0)
   (accessors nil))
+
+(defun make-attr (attr-set attr-name &rest attr-data)
+  (if (nth-value 1 (gethash attr-name attr-set))
+      (error "Attribute ~A is defined more than once." attr-name)
+      (apply #'%make-attr attr-data)))
+
+(defun make-attr-set (&rest attr-specs)
+  (let ((attr-set (make-hash-table)))
+    (loop :for (name . data) :in attr-specs
+          :for attr = (apply #'make-attr attr-set name data)
+          :do (setf (gethash name attr-set) attr))
+    attr-set))
 
 (defun attr-type-valid-p (type gl-version)
   "Check whether the given attribute type and OpenGL version is legal."
