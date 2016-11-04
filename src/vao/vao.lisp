b0cbbdc5120ec36998988f66c5d7d9ef6204eb06
Michael Fiano <michael.fiano@gmail.com>
2016-11-03 21:11:50 -0400

    Added add/remove attribute functions, a convenience macro for defining sets of attributes, and a comment documenting the usage.

--- src/vao/vao.lisp
+++ src/vao/vao.lisp
@@ -1,27 +1,42 @@
 (in-package :gloss.vao)
 
-(defstruct (attr (:constructor %make-attr))
+;;; VAO Attributes
+
+(defstruct (attribute (:constructor %make-attribute))
   (type :float)
   (count 1)
   (normalizep nil)
   (divisor 0)
   (accessors nil))
 
-(defun make-attr (attr-set attr-name &rest attr-data)
-  (if (nth-value 1 (gethash attr-name attr-set))
-      (error "Attribute ~A is defined more than once." attr-name)
-      (apply #'%make-attr attr-data)))
+(defun make-attribute (attribute-set attribute-name &rest attribute-data)
+  (if (nth-value 1 (gethash attribute-name attribute-set))
+      (error "Attribute ~A is defined more than once." attribute-name)
+      (apply #'%make-attribute attribute-data)))
 
-(defun make-attr-set (&rest attr-specs)
-  (let ((attr-set (make-hash-table)))
-    (loop :for (name . data) :in attr-specs
-          :for attr = (apply #'make-attr attr-set name data)
-          :do (setf (gethash name attr-set) attr))
-    attr-set))
+(defun add-attributes (attribute-set &rest attribute-specs)
+  (loop :for (name . data) :in attribute-specs
+        :for attr = (apply #'make-attribute attribute-set name data)
+        :do (setf (gethash name attribute-set) attr))
+  attribute-set)
+
+(defun remove-attributes (attribute-set &rest attribute-names)
+  (loop :for name :in attribute-names
+        :do (remhash name attribute-set))
+  attribute-set)
+
+(defun %make-attribute-set (&rest attribute-specs)
+  (let ((attribute-set (make-hash-table)))
+    (apply #'add-attributes attribute-set attribute-specs)
+    attribute-set))
+
+(defmacro make-attribute-set (options &body attribute-specs)
+  (declare (ignore options))
+  `(apply #'%make-attribute-set ',attribute-specs))
 
 ;; type-checking validity table
 ;; this is just to remind what needs to be type-checked later on
-(defvar *attr-validity-table*
+#++(defvar *attr-validity-table*
   '((:half-float . (:version 3.0))
     (:float . (:version 3.0))
     (:byte . (:version 3.0))
@@ -33,3 +48,17 @@
     (:int-2-10-10-10-rev . (:version 3.2 :count 4))
     (:unsigned-int-2-10-10-10-rev . (:version 3.2 :count 4))
     (:unsigned-int-10f-11f-11f-rev . (:version 4.4 :count 3))))
+
+;; usage make-attribute-set is a macro that so far just allows defining
+;; attribute specs the same way as its functional expansion,
+;; %make-attribute-set, except without QUOTE'ing each spec.
+#++(let ((attr-set
+        (make-attribute-set ()
+          (position :type :float :count 3 :accessors (px py pz))
+          (normals :type :float :count 3 :accessors (nx ny nz))
+          (uvs :type :float :count 3 :accessors (uvx uvy uvz)))))
+  (add-attributes attr-set
+                  '(some-attr-1 :type :byte :count 2)
+                  '(some-attr-2 :type :unsigned-byte :count 2))
+  (remove-attributes attr-set 'some-attr-1)
+  attr-set)
