ac4f0d1e3a6d3e6ebda5aafc62c914d5d1389cf8
Peter Keller <psilord@cs.wisc.edu>
2016-11-13 21:38:53 -0600

    This is an experimental rewrite of gloss-message that exists side by side with gloss-message. It is to see if we like it better or not.

--- src/vao/conditions.lisp
+++ src/vao/conditions.lisp
@@ -1,5 +1,40 @@
 (in-package :gloss.vao)
 
+(defparameter *gloss-message-db*
+  (let ((db (make-hash-table)))
+    (mapcar
+     (lambda (entry)
+       (destructuring-bind (kind format-string) entry
+         (setf (gethash kind db) format-string)))
+     ;; Hrm, this representation allows spreading this stuff across files....
+     '((attribute-name-duplicated
+        "Attribute name is defined more than once.
+These attributes have duplicate names:~%~(~{  ~S~%~}~)")
+       (attribute-locations-undefined
+        "All attributes in this set have no :LOCATION defined.
+These attributes have no :LOCATION definitions:~%~(~{  ~S~%~}~)")
+       (attribute-location-partially-defined
+        "If any attribute has a :LOCATION, all other attributes must also.
+These attributes need their locations defined:~%~(~{  ~S~%~}~)")
+       (attribute-location-type-error
+        "Attribute value for :LOCATION must be an integer.
+These attributes do not have integer :LOCATION values:~%~(~{  ~S~%~}~)")
+       (attribute-location-range-error
+        "Attribute value :LOCATION must not be negative.
+These attributes have negative :LOCATION values:~%~(~{  ~S~%~}~)")
+       (attribute-locations-defined
+        "All attributes in this set use :LOCATION properly.
+These attributes use :LOCATION properly: ~(~{  ~S~%~}~)")))
+    db))
+
+(defun gloss-message-new (kind)
+  (multiple-value-bind (message presentp) (gethash kind *gloss-message-db*)
+    (if presentp
+        message
+        (format nil "Unknown gloss-message kind: ~A" kind))))
+
+
+
 (defmethod gloss-message ((kind (eql 'attribute-name-duplicated)))
   "Attribute name is defined more than once.
 These attributes have duplicate names:~%~(~{  ~S~%~}~)")
