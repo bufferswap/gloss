3e8096849d42595446039a34f2ea349b1ed93322
Michael Fiano <michael.fiano@gmail.com>
2016-11-14 03:26:39 -0500

    Refactored attributes some to try to learn how it works and make it a little nicer.

--- src/vao/attributes.lisp
+++ src/vao/attributes.lisp
@@ -25,73 +25,61 @@
          (bail (analysis kind context)
            (return-from %analyze-attribute-locations
              (values analysis kind context)))
-         (select-if (filter lst &key (transform #'identity))
-           (mapcar transform (remove-if (complement filter) lst))))
-
-    ;; Check for duplicate attribute names.
-    (let ((duplicate-count (make-hash-table)))
-      (loop :for attr-name :in (mapcar #'first spec) :do
-         (if (null (nth-value 1 (gethash attr-name duplicate-count)))
-             (setf (gethash attr-name duplicate-count) 1)
-             (incf (gethash attr-name duplicate-count))))
-      (let ((duplicates ()))
-        (maphash (lambda (name use-count)
-                   (when (> use-count 1)
-                     (push name duplicates)))
-                 duplicate-count)
-        (when duplicates
-          (bail :error
-                'attribute-name-duplicated
-                (list (select-if (lambda (attr)
-                                   (when (member (first attr) duplicates)
-                                     attr))
-                                 spec))))))
-
-    ;; Check that no :locations are defined.
-    (unless (find :location spec
-                  :test (lambda (item attr) (member item attr)))
+         (select-if (filter lst)
+           (mapcar #'identity (remove-if (complement filter) lst))))
+    ;; Check for duplicate attribute names
+    (let ((duplicate-attrs))
+      (loop :with count = (make-hash-table)
+            :for (name . nil) :in spec
+            :if (nth-value 1 (gethash name count))
+              :do (incf (gethash name count))
+                  (push name duplicate-attrs)
+            :else
+              :do (setf (gethash name count) 1))
+      (when duplicate-attrs
+        (bail :error
+              'attribute-name-duplicated
+              (list (select-if (lambda (attr)
+                                 (member (first attr) duplicate-attrs))
+                               spec)))))
+    ;; Check attribute locations
+    (unless (some #'has-location-p spec)
       (bail :undefined
             'attribute-locations-undefined
             (list spec)))
-    ;; Check that all :locations are defined
     (when (/= (length spec) (count-if #'has-location-p spec))
       (bail :error
             'attribute-location-partially-defined
             (list (select-if (complement #'has-location-p) spec))))
-    ;; Check that all :locations are integers.
-    (when (find-if (complement #'location-integerp) spec)
+    (unless (every #'location-integerp spec)
       (bail :error
             'attribute-location-type-error
             (list (select-if (complement #'location-integerp) spec))))
-    ;; Check that all :locations are not negative.
-    (when (find-if #'location-minusp spec)
+    (when (some #'location-minusp spec)
       (bail :error
             'attribute-location-range-error
             (list (select-if #'location-minusp spec))))
-    ;; If all the above passes, then :locations are defined.
     (values :defined
             'attribute-locations-defined
             (list spec))))
 
 (defun %assign-attribute-locations (attribute-set)
   (loop :for (name . properties) :in (spec attribute-set)
-     :for attr = (gethash name (attributes attribute-set))
-     :for location :from 0
-     :do (setf (attr-location attr) location
-               (getf properties :location) location)))
+        :for attr = (gethash name (attributes attribute-set))
+        :for location :from 0
+        :do (setf (attr-location attr) location)))
 
 (defun make-attribute-set (&rest spec)
   (let ((attribute-set (%make-attribute-set :spec (copy-seq spec))))
-
     (multiple-value-bind (analysis kind context)
         (%analyze-attribute-locations spec)
       (ecase analysis
         ((:undefined :defined)
          (loop :with attributes = (attributes attribute-set)
-            :for (name . properties) :in spec
-            :for attr = (apply #'%make-attribute :name name properties)
-            :do (setf (gethash name attributes) attr))
-         (when :undefined
+               :for (name . properties) :in spec
+               :for attr = (apply #'%make-attribute :name name properties)
+               :do (setf (gethash name attributes) attr))
+         (when (eq analysis :undefined)
            (%assign-attribute-locations attribute-set)))
         (:error
          (gloss-error kind context))))
